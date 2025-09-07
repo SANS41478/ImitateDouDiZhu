d552a719d61ffdafbdc317463b6c68e2ae5ffe36
乡村Meg <146180040+SANS41478@users.noreply.github.com>
2025-09-07 15:44:51 +0800

    audioend

--- Assets/Scripts/3Controller/BuChuCommand.cs
+++ Assets/Scripts/3Controller/BuChuCommand.cs
@@ -2,13 +2,29 @@ using strange.extensions.command.impl;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BuChuCommand : EventCommand
 {
     [Inject]
     public RoundModel RoundModel { get; set; }
+    private System.Random random = new System.Random();
+    
     override public void Execute()
     {
         RoundModel.Turn();
+        int delay = random.Next(0,3);
+        switch (delay) 
+        {
+            case 0:
+                AudioManager.Instance.PlaySFX("buyao");
+                break;
+            case 1:
+                AudioManager.Instance.PlaySFX("guo");
+                break;
+            case 2:
+                AudioManager.Instance.PlaySFX("yaobuqi");
+                break;
+        }
     }
 }
